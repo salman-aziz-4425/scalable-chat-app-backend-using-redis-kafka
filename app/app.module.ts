0aa402132e26407be1783c87a4d01a9710c58491
MacBook <macbook@Salman-Aziz.local>
2024-07-15 02:53:22 +0500

    fix: some fixes

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { ChatGateway } from 'chat/chat.gateway';
 import { ChatModule } from 'chat/chat.module';
 import { DatabaseModule } from 'lib/commons/src/database/database.module';
 import { UserModule } from 'user/src/user.module';
@@ -7,6 +6,6 @@ import { UserModule } from 'user/src/user.module';
 @Module({
   imports: [DatabaseModule,UserModule,ChatModule],
   controllers: [],
-  providers: [ChatGateway],
+  providers: [],
 })
 export class AppModule {}
