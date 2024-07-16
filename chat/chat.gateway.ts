c15380483e474d3297198ecea1a657207de3b3eb
MacBook <macbook@Salman-Aziz.local>
2024-07-16 19:44:29 +0500

    fix: some fixes

--- chat/chat.gateway.ts
+++ chat/chat.gateway.ts
@@ -11,7 +11,8 @@ import { Server, Socket } from 'socket.io';
 export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer() server: Server;
 
-  private connectedUsers: Map<string, string> = new Map();
+  private connectedUsers: Map<string, string[]> = new Map();
+  
 
   afterInit(server: Server) {
     console.log('WebSocket Gateway Initialized');
@@ -19,59 +20,80 @@ export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
 
   handleConnection(client: Socket) {
     console.log(`Client connected: ${client.id}`);
+    this.broadcastActiveUsers();
   }
 
   handleDisconnect(client: Socket) {
     console.log(`Client disconnected: ${client.id}`);
-    const userEmail = this.connectedUsers.get(client.id);
-    if (userEmail) {
-      this.connectedUsers.delete(client.id);
-      this.broadcastActiveUsers();
-    }
+    this.removeSocketIdFromEmail(client.id);
+    this.broadcastActiveUsers();
   }
 
   @SubscribeMessage('user_online')
   handleUserOnline(client: Socket, payload: { email: string }): void {
     console.log(`${payload.email} is online`);
+
     if (!this.connectedUsers.has(payload.email)) {
-      this.connectedUsers.set(payload.email, client.id);
-      this.broadcastActiveUsers();
+      this.connectedUsers.set(payload.email, [client.id]);
     } else {
-      console.log(`${payload.email} is already registered`);
+      const socketIds = this.connectedUsers.get(payload.email);
+      if (socketIds && !socketIds.includes(client.id)) {
+        socketIds.push(client.id);
+        this.connectedUsers.set(payload.email, socketIds);
+      }
     }
+    this.broadcastActiveUsers();
   }
 
   @SubscribeMessage('user_offline')
   handleUserOffline(client: Socket, payload: { email: string }): void {
     console.log(`${payload.email} is offline`);
-    if (this.connectedUsers.has(payload.email)) {
-      this.connectedUsers.delete(payload.email);
-      this.broadcastActiveUsers();
-    } else {
-      console.log(`${payload.email} was not registered`);
-    }
+    this.removeSocketIdFromEmail(client.id);
+    this.broadcastActiveUsers();
   }
 
   @SubscribeMessage('send_message')
   handleMessage(client: Socket, payload: { sender: string, recipient: string, message: string }): void {
     const { sender, recipient, message } = payload;
-    const messageObject = { id: new Date().getTime(), sender,recipient, message };
-    const recipientSocketId = this.connectedUsers.get(recipient);
-    console.log("helo")
-    console.log(recipient)
-    if (recipientSocketId) {
-      this.server.to(recipientSocketId).emit('receive_message', messageObject);
+    const messageObject = { id: new Date().getTime(), sender, recipient, message };
+    const recipientSocketIds = this.getSocketIdsByEmail(recipient);
+    const senderSocketIds = this.getSocketIdsByEmail(sender);
+    const recipents=recipientSocketIds?[...recipientSocketIds,...senderSocketIds]:[]
+    if (recipents && recipents.length > 0) {
+      recipents.forEach(socketId => {
+        if(socketId!==client.id){
+          this.server.to(socketId).emit('receive_message', messageObject);
+        }
+      });
       console.log(`Sent message to ${recipient}: ${message}`);
     } else {
       console.log(`Recipient ${recipient} is not online`);
     }
   }
+
   private broadcastActiveUsers(): void {
-    console.log(this.connectedUsers)
-    const activeUsersSet = new Set(this.connectedUsers.keys());
-    const activeUsers = Array.from(activeUsersSet);
-    console.log('Broadcasting active users:', activeUsers);
+    const activeUsers = Array.from(this.connectedUsers.keys());
+    console.log("users broadcasted")
+    console.log(this.connectedUsers);
     this.server.emit('active_user', activeUsers);
   }
 
+  private getSocketIdsByEmail(email: string): string[] | undefined {
+    return this.connectedUsers.get(email);
+  }
+
+  private removeSocketIdFromEmail(socketId: string): void {
+    for (const [email, socketIds] of this.connectedUsers.entries()) {
+      const index = socketIds.indexOf(socketId);
+      if (index !== -1) {
+        socketIds.splice(index, 1);
+        if (socketIds.length === 0) {
+          this.connectedUsers.delete(email);
+        } else {
+          this.connectedUsers.set(email, socketIds);
+        }
+        break;
+      }
+    }
+  }
 }
