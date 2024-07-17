ffc63d06b67db1bc4f3e3752348fea5e764969d5
MacBook <macbook@Salman-Aziz.local>
2024-07-18 04:31:40 +0500

    feat: implment ui changes

--- chat/chat.gateway.ts
+++ chat/chat.gateway.ts
@@ -1,5 +1,4 @@
 import Redis from 'ioredis';
-
 import { ConfigService } from '@nestjs/config';
 import { InjectRepository } from '@nestjs/typeorm';
 import { OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
@@ -19,37 +18,38 @@ import { Message } from 'user/src/models/message.model';
 export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer() server: Server;
 
-  private connectedUsers: Map<string, string[]> = new Map();
   private pub: Redis;
   private sub: Redis;
-  private producer:Producer;
-  private cosumer: Consumer;
+  private producer: Producer;
+  private consumer: Consumer;
 
-  constructor(private readonly configService: ConfigService,
+  constructor(
+    private readonly configService: ConfigService,
     @InjectRepository(Message)
-    private readonly messageRepository: Repository<Message>) {
+    private readonly messageRepository: Repository<Message>,
+  ) {
     const redisInfo = {
       host: this.configService.get<string>('REDIS_DATABASE_HOST'),
       port: this.configService.get<number>('REDIS_DATABASE_PORT'),
       username: this.configService.get<string>('REDIS_DATABASE_USERNAME'),
       password: this.configService.get<string>('REDIS_DATABASE_PASSWORD'),
-    }
+    };
     this.pub = new Redis(redisInfo);
     this.sub = new Redis(redisInfo);
 
     const kafkaConfig: KafkaConfig = {
       clientId: 'test-app',
-      brokers: ['localhost:9092']
-    }
-    const kafka = new Kafka(kafkaConfig)
+      brokers: ['localhost:9092'],
+    };
+    const kafka = new Kafka(kafkaConfig);
 
-    this.producer = kafka.producer()
-    this.cosumer = kafka.consumer({ groupId: 'test-group' })
+    this.producer = kafka.producer();
+    this.consumer = kafka.consumer({ groupId: 'test-group' });
 
-    this.producer.connect()
-    this.cosumer.connect()
+    this.producer.connect();
+    this.consumer.connect();
 
-    this.cosumer.subscribe({ topic: 'chat-topic', fromBeginning: true });
+    this.consumer.subscribe({ topic: 'chat-topic', fromBeginning: true });
     this.sub.subscribe('user_online', 'user_offline', 'send_message');
     this.sub.on('message', (channel, message) => {
       const payload = JSON.parse(message);
@@ -65,19 +65,18 @@ export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
           break;
       }
     });
-    this.cosumer.run({
-      eachMessage: async ({ topic, partition, message, pause }:EachMessagePayload) => {
-        const messageObject:any = JSON.parse(message.value as any)
+    this.consumer.run({
+      eachMessage: async ({ topic, partition, message, pause }: EachMessagePayload) => {
+        const messageObject: any = JSON.parse(message.value as any);
         try {
           const timestamp = new Date();
-          await this.messageRepository.insert({ sender: messageObject.sender, recipient: messageObject.recipient, message: messageObject.message, timestamp })
+          await this.messageRepository.insert({ sender: messageObject.sender, recipient: messageObject.recipient, message: messageObject.message, timestamp });
         } catch (error) {
-          console.log(error)
-          pause()
-          setTimeout(() => { this.cosumer.resume([{ topic: "chat-topic" }]) }, 60 * 10000)
+          console.log(error);
+          pause();
+          setTimeout(() => { this.consumer.resume([{ topic: "chat-topic" }]); }, 60 * 10000);
         }
-
-        console.log("data inserted")
+        console.log("data inserted");
       },
     });
   }
@@ -88,24 +87,27 @@ export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
 
   handleConnection(client: Socket) {
     console.log(`Client connected: ${client.id}`);
-    this.broadcastActiveUsers();
   }
 
   handleDisconnect(client: Socket) {
     console.log(`Client disconnected: ${client.id}`);
-    this.removeSocketIdFromEmail(client.id);
-    this.broadcastActiveUsers();
+    this.handleUserOffline(client);
   }
 
   @SubscribeMessage('user_online')
   handleUserOnline(client: Socket, payload: { email: string }): void {
     console.log(`${payload.email} is online`);
-    this.pub.publish('user_online', JSON.stringify({...payload,id:client.id}));
+    this.pub.publish('user_online', JSON.stringify({ ...payload, id: client.id }));
   }
 
   @SubscribeMessage('user_offline')
-  handleUserOffline(client: Socket, payload: { email: string }): void {
-    this.pub.publish('user_offline', JSON.stringify({...payload,id:client.id}));
+  handleUserOffline(client: Socket, payload?: { email: string }): void {
+    if (!payload) {
+      this.removeSocketIdFromEmail(client.id);
+      this.broadcastActiveUsers();
+    } else {
+      this.pub.publish('user_offline', JSON.stringify({ ...payload, id: client.id }));
+    }
   }
 
   @SubscribeMessage('send_message')
@@ -116,62 +118,28 @@ export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
     this.producer.send({
       topic: 'chat-topic',
       messages: [{
-        value: JSON.stringify({ ...messageObject })
-      }]
-    })
+        value: JSON.stringify({ ...messageObject }),
+      }],
+    });
   }
 
-  private broadcastActiveUsers(): void {
-    const activeUsers = Array.from(this.connectedUsers.keys());
-    console.log('Users broadcasted');
-    console.log(this.connectedUsers);
-    console.log(activeUsers);
-    this.server.emit('active_user', activeUsers);
-  }
-
-  private getSocketIdsByEmail(email: string): string[] | undefined {
-    return this.connectedUsers.get(email);
-  }
-
-  private removeSocketIdFromEmail(socketId: string): void {
-    for (const [email, socketIds] of this.connectedUsers.entries()) {
-      const index = socketIds.indexOf(socketId);
-      if (index !== -1) {
-        socketIds.splice(index, 1);
-        if (socketIds.length === 0) {
-          this.connectedUsers.delete(email);
-        } else {
-          this.connectedUsers.set(email, socketIds);
-        }
-        break;
-      }
-    }
-  }
-
-  private handleUserOnlineRedis(payload: { email: string,id:string }): void {
-    if (!this.connectedUsers.has(payload.email)) {
-      this.connectedUsers.set(payload.email, [payload.id]);
-    } else {
-      const socketIds = this.connectedUsers.get(payload.email);
-      if (socketIds && !socketIds.includes(payload.id)) {
-        socketIds.push(payload.id);
-        this.connectedUsers.set(payload.email, socketIds);
-      }
-    }
+  private async handleUserOnlineRedis(payload: { email: string, id: string }): Promise<void> {
+    await this.pub.sadd(`user_socket:${payload.email}`, payload.id);
     this.broadcastActiveUsers();
   }
 
-  private handleUserOfflineRedis(payload: { email: string ,id:string}): void {
+  private async handleUserOfflineRedis(payload: { email: string, id: string }): Promise<void> {
     console.log(`${payload.email} is offline`);
-    this.removeSocketIdFromEmail(payload.id);
+    await this.pub.srem(`user_socket:${payload.email}`, payload.id);
     this.broadcastActiveUsers();
   }
 
-  private handleMessageRedis(payload: { id: number, sender: string, recipient: string, message: string, clientId: string }): void {
-    const { sender, recipient,clientId } = payload;
-    const recipientSocketIds = this.getSocketIdsByEmail(recipient);
-    const senderSocketIds = this.getSocketIdsByEmail(sender);
-    const recipients = recipientSocketIds ? [...recipientSocketIds, ...senderSocketIds] : [];
+  private async handleMessageRedis(payload: { id: number, sender: string, recipient: string, message: string, clientId: string }): Promise<void> {
+    console.log("message");
+    const { sender, recipient, clientId } = payload;
+    const recipientSocketIds = await this.pub.smembers(`user_socket:${recipient}`);
+    const senderSocketIds = await this.pub.smembers(`user_socket:${sender}`);
+    const recipients = [...recipientSocketIds, ...senderSocketIds];
 
     if (recipients && recipients.length > 0) {
       recipients.forEach(socketId => {
@@ -182,4 +150,26 @@ export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
     }
   }
 
+  private async broadcastActiveUsers(): Promise<void> {
+    const keys = await this.pub.keys('user_socket:*');
+    const activeUsers = keys.map(key => key.split(':')[1]);
+    console.log('Users broadcasted');
+    console.log(activeUsers);
+    this.server.emit('active_user', activeUsers);
+  }
+
+  private async removeSocketIdFromEmail(socketId: string): Promise<void> {
+    const keys = await this.pub.keys('user_socket:*');
+    for (const key of keys) {
+      const email = key.split(':')[1];
+      const socketIds = await this.pub.smembers(key);
+      if (socketIds.includes(socketId)) {
+        await this.pub.srem(key, socketId);
+        if ((await this.pub.scard(key)) === 0) {
+          await this.pub.del(key);
+        }
+        break;
+      }
+    }
+  }
 }
